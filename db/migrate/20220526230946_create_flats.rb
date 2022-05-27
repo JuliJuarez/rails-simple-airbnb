365f917f110804b015e4a388fbb5a1a2a972d607
juliana juarez <juulijuarez@gmail.com>
2022-05-26 22:01:38 -0300

    Style

--- db/migrate/20220526230946_create_flats.rb
+++ db/migrate/20220526230946_create_flats.rb
@@ -2,10 +2,11 @@ class CreateFlats < ActiveRecord::Migration[6.1]
   def change
     create_table :flats do |t|
       t.string :name
-      t.string :addres
+      t.string :address
       t.text :description
       t.integer :price_per_night
       t.integer :number_of_guests
+      t.string :picture_url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,11 @@ ActiveRecord::Schema.define(version: 2022_05_26_230946) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
-    t.string "addres"
+    t.string "address"
     t.text "description"
     t.integer "price_per_night"
     t.integer "number_of_guests"
+    t.string "picture_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
